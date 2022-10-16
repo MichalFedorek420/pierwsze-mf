83efe18b26786bccb6c5281840c31333f98311e3
MichalFedorek420 <michal.fedorek2@gmail.com>
2022-10-16 18:09:45 +0200

    zadanie 15

---
+++ 02-TypesAndVariables/zad15.py
@@ -0,0 +1,21 @@
+# The product of 15 and 38
+print("The product of 15 and 38:", 15*38)
+# The product of the sum of the pairs of numbers 3 and 4, and 5 and 9
+a = 3+4
+b = 5+9
+c = a*b
+print(c)
+# Integer part of dividing the numbers 7 and 2
+print(7//2)
+# The remainder of the division of 48 and 5
+print(48%5)
+# Arithmetic mean of the numbers 8, 7, 4, 2
+a = 8+7+4+2
+b = a/4
+print(b)
+# 210
+print(2**10)
+# Square root of 49 (do not use a function)
+
+# 25% of 80
+print(25/100*80)
