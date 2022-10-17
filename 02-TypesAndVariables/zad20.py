88883b4c559f57ef938b82be3350d2683a82a07c
MichalFedorek420 <michal.fedorek2@gmail.com>
2022-10-17 11:31:36 +0200

    zad19

---
+++ 02-TypesAndVariables/zad20.py
@@ -0,0 +1,22 @@
+import random
+
+prob = random.random()
+print(prob)
+
+diceThrow = random.randrange(1, 7)       # return an int, one of 1,2,3,4,5,6
+print(diceThrow)
+import random
+
+prob = random.random()
+print(prob)
+
+diceThrow = random.randrange(1, 7)       # return an int, one of 1,2,3,4,5,6
+print(diceThrow)
+import random
+
+prob = random.random()
+print(prob)
+
+diceThrow = random.randrange(1, 7)       # return an int, one of 1,2,3,4,5,6
+print(diceThrow)
+print({diceThrow}+{diceThrow+diceThrow})
