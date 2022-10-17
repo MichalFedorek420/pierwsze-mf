f90e05d7d3f3deabc7ad2390bc1fb4891de671d9
MichalFedorek420 <michal.fedorek2@gmail.com>
2022-10-17 12:12:29 +0200

    zad2

--- 03/zad1.py
+++ 03/zad1.py
@@ -1,6 +1,10 @@
-kasa = int(input("ile zarabiasz"))
-godziny = int(input("ile przepracowales"))
-if godziny <= 40:
-    print(f'zarobiłeś {godziny*kasa}')
-else:
-    print(f'zarobiłeś {40*kasa+((godziny-40)*kasa*3/2)}') 
+try:
+    kasa = int(input("ile zarabiasz"))
+    godziny = int(input("ile przepracowales"))
+    if godziny <= 40:
+        print(f'zarobiłeś {godziny*kasa}')
+    else:
+        print(f'zarobiłeś {40*kasa+((godziny-40)*kasa*3/2)}')
+except ValueError:
+    print("dawaj liczbe ziomek")
+    
